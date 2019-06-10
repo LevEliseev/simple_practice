065a595bd3c6498ef28659dea7e290c6098ed7f8
Lev Yeliseev <yeliseev.lev@gmail.com>
2019-06-11 04:25:43 +0500

    new tutorial

---
+++ hackerrank/tutorials/10_days_of_javascript/day2_loops.js
@@ -0,0 +1,38 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the vowelsAndConsonants function.
+ * Print your output using 'console.log()'.
+ */
+function vowelsAndConsonants(s) {
+    s.match(/[aeiou]/gi).concat(s.match(/[^aeiou]/gi)).forEach(e => console.log(`${e}`));
+}
+
+
+function main() {
+    const s = readLine();
+    
+    vowelsAndConsonants(s);
+}
