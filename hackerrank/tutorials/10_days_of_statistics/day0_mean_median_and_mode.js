970d7c1a4ae847f7636abb993c3d06c6e48039ee
Lev Yeliseev <yeliseev.lev@gmail.com>
2019-06-13 13:54:16 +0500

    small fix

--- hackerrank/tutorials/10_days_of_statistics/day0_mean_median_and_mode.js
+++ hackerrank/tutorials/10_days_of_statistics/day0_mean_median_and_mode.js
@@ -1,17 +1,20 @@
 function processData(input) {
     //Enter your code here
+    //input
     let ind = input.indexOf('\n');
     input = input.substring(ind);
     input = input.split(' ');
     for (let i = 0; i < input.length; i++) input[i] = +input[i];
+    //solution
     let mean, median, mode;
-    mean = input.reduce((a, b) => a + b) / input.length;
-    mean = Math.round(mean * 10) / 10;
+    //mean
+    mean = (input.reduce((a, b) => a + b) / input.length).toFixed(1);
+    //median
     let sorted = input.sort((a, b) => a - b);
-    if (input.length % 2 != 0) median = input[Math.floor(input.length / 2)];
-    else median = (input[input.length / 2] + input[input.length / 2 - 1]) / 2;
-    median = Math.round(median * 10) / 10;
-    let counter = 1, o = {};
+    if (input.length % 2 != 0) median = (input[Math.floor(input.length / 2)]).toFixed(1);
+    else median = ((input[input.length / 2] + input[input.length / 2 - 1]) / 2).toFixed(1);
+    //mode
+    let counter = 1, o = {}, max = 0;
     for (let i = 0; i < sorted.length - 1; i++) {
         while (sorted[i] == sorted[i + 1]) {
             counter++;
@@ -19,23 +22,16 @@ function processData(input) {
         }
         
         o[sorted[i]] = counter;
-        //arr.push(counter);
-        //dynamic array
-        //rotate
-        //repeated string
+        if (counter > max) max = counter;
         counter = 1;
     }
-    let arr = [];
-    for (let prop in o) {
-        arr.push(o[prop]);
-    }
-    let max = Math.max(...arr);
     for (let prop in o) {
         if (o[prop] == max) {
             mode = prop;
             break;
         }
     }
+    //output
     console.log(mean);
     console.log(median);
     console.log(mode);
---
+++ hackerrank/tutorials/30_days_of_code/day2_operators.js
@@ -0,0 +1,39 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the solve function below.
+function solve(meal_cost, tip_percent, tax_percent) {
+    meal_cost += meal_cost * (tip_percent / 100) + meal_cost * (tax_percent / 100);
+    console.log(Math.round(meal_cost));
+}
+
+function main() {
+    const meal_cost = parseFloat(readLine());
+
+    const tip_percent = parseInt(readLine(), 10);
+
+    const tax_percent = parseInt(readLine(), 10);
+
+    solve(meal_cost, tip_percent, tax_percent);
+}
