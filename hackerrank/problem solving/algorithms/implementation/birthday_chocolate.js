08f34135a2151cd2899c2e6cafd8f5125b0b282b
Lev Yeliseev <yeliseev.lev@gmail.com>
2019-06-23 15:47:36 +0500

    day 12 tutorial & probl solv

---
+++ hackerrank/problem solving/algorithms/implementation/birthday_chocolate.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the birthday function below.
+function birthday(s, d, m) {
+    let counter = 0, sum;
+    for (let i = 0; i <= s.length - m; i++) {
+        sum = s[i];
+        for (let j = i + 1; j < i + m; j++) {
+            sum += s[j];
+        }
+        if (sum === d) counter++;
+    }
+    return counter;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const s = readLine().replace(/\s+$/g, '').split(' ').map(sTemp => parseInt(sTemp, 10));
+
+    const dm = readLine().replace(/\s+$/g, '').split(' ');
+
+    const d = parseInt(dm[0], 10);
+
+    const m = parseInt(dm[1], 10);
+
+    const result = birthday(s, d, m);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
